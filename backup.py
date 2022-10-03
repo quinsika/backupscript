fb44507989449509a31a7bdf5cfae72181d067f9
quuinsika <quinsika.sharma@gmail.com>
2022-10-03 15:46:09 -0700

    my second commit

--- backup.py
+++ backup.py
@@ -5,6 +5,10 @@ def backupfunc():
     subprocess.run(["sleep", "5"])
     print ("backup complete")
 
+def move_to_s3():
+    print("moving to s3")
+
 if __name__ == "__main__":
     backupfunc()
+    move_to_s3()
 
